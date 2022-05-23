7e1619a141c56c8f40dc40dff8a465fb028388e6
Govinda G <99521156+govindarajug@users.noreply.github.com>
2022-05-23 13:25:39 +0530

    Changed parsing to use iterator
    * working when only filename is given
    * working if option and value is given

--- src/parseArgs.js
+++ src/parseArgs.js
@@ -1,20 +1,50 @@
-const isOptionInvalid = option => !option.startsWith('-');
+const argsIterator = function (args) {
+  const currentArg = function () {
+    return args[this.index];
+  };
+  const nextArg = function () {
+    this.index++;
+    return args[this.index];
+  };
+  const hasMoreArgs = function () {
+    return this.index < this.args.length;
+  };
+  const iterator = {};
+  iterator.index = 0;
+  iterator.currentArg = currentArg.bind(iterator);
+  iterator.nextArg = nextArg.bind(iterator);
+  iterator.hasMoreArgs = hasMoreArgs.bind(iterator);
+  iterator.args = args;
+  return iterator;
+};
+
+const parseFileName = function (iterator) {
+  return iterator.args[iterator.index];
+};
+
+const parseOption = function (iterator) {
+  let option = 'lines';
+  if (iterator.currentArg() === '-c') {
+    option = 'bytes';
+  }
+  const count = +iterator.nextArg();
+  return { option, count };
+};
 
 const parseArgs = function (args) {
-  const fileName = args[args.length - 1];
-  const keys = { '-n': 'lines', '-c': 'bytes' };
-  const options = { option: 'lines', count: 10 };
-  
-  for (let index = 0; index < args.length - 1; index += 2) {
-    const [option, value] = args.slice(index, index + 2);
-    if (isOptionInvalid(option)) {
-      throw {
-        message: '-- illegal option'
-      };
-    }
-    options.option = keys[option];
-    options.count = isFinite(value) ? +value : value;
+  let options = { option: 'lines', count: 10 };
+  const usage = 'usage: head [-n lines | -c bytes] [file ...]';
+  if (args.length === 0) {
+    throw {
+      usage
+    };
   }
+  const iterator = argsIterator(args);
+  while (iterator.hasMoreArgs() && iterator.currentArg().startsWith('-')) {
+    options = parseOption(iterator);
+    iterator.index++;
+  }
+  const fileName = parseFileName(iterator);
   return { fileName, options };
 };
 
--- test/testParseArgs.js
+++ test/testParseArgs.js
@@ -2,11 +2,15 @@ const assert = require('assert');
 const { parseArgs } = require('../src/parseArgs.js');
 
 describe('parseArgs', () => {
-  it('Should parse just the file name', () => {
+  it.only('Should show usage when no args are given', () => {
+    assert.throws(() => parseArgs([]), 'usage: head [-n lines | -c bytes] [file ...]');
+  });
+
+  it.only('Should parse just the file name', () => {
     assert.deepStrictEqual(parseArgs(['a.txt']), { fileName: 'a.txt', options: { option: 'lines', count: 10 } });
   });
 
-  it('Should parse -n option along with the file name', () => {
+  it.only('Should parse -n option along with the file name', () => {
     assert.deepStrictEqual(parseArgs(['-n', '2', 'a.txt']), { fileName: 'a.txt', options: { option: 'lines', count: 2 } });
   });
   
@@ -15,7 +19,7 @@ describe('parseArgs', () => {
     assert.deepStrictEqual(parseArgs(['-c', '2', '-c', '1', 'a.txt']), { fileName: 'a.txt', options: { option: 'bytes', count: 1 } });
   });
 
-  it('Should parse -c option along with the file name', () => {
+  it.only('Should parse -c option along with the file name', () => {
     assert.deepStrictEqual(parseArgs(['-c', '3', 'a.txt']), { fileName: 'a.txt', options: { option: 'bytes', count: 3 } });
   });
 
